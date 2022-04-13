9965ed16172a5d07fca4f3168a2913d25f08068b
Jason Clark <jason.o.clark90@gmail.com>
2022-04-13 10:34:40 -0400

    Added Geolocation and GMap

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,8 +5,18 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import VueGeolocation from 'vue-browser-geolocation'
 
 Vue.config.productionTip = false
+Vue.use(VueGeolocation)
+
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps,  {
+  load: {
+    key: 'AIzaSyBnRbD3j0M-BIHvJ6vVwTpe7iVOxZGNuEo'
+  },
+})
+
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -5,11 +5,17 @@
     <p>
       In the initial configuration, this page can only be seen when logged in.
     </p>
+    <Map/>
   </div>
 </template>
 
 <script>
+import Map from '../components/Map.vue';
 export default {
   name: "home",
-};
+  components: {
+    Map
+  }
+
+  };
 </script>
