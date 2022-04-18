0f7a13d438908be25c93c328dd1a99d049b99850
Awel Alin <awel337@gmail.com>
2022-04-18 08:56:37 -0400

    changes in My IyItinerary

--- vue/src/components/MyItinerary.vue
+++ vue/src/components/MyItinerary.vue
@@ -4,37 +4,52 @@
   </div>
   <div v-else>
     <h2>{{ itinerary.itineraryName }}</h2>
-    </div>
+    
+      <landmarks-overview />
+    
+  </div>
 </template>
 
 <script>
 import itineraryService from "../services/ItineraryService.js";
+import LandmarksOverview from "../components/LandmarksOverview.vue";
+
 export default {
- data() {
+  components: { LandmarksOverview },
+  data() {
     return {
       isLoading: true,
+      inItinerary: true,
     };
   },
+
   methods: {
     getUserItinerary() {
       itineraryService
-        .getItinerary(this.$route.params.itineraryId).then((response) => {
-        this.$store.commit("SET_CURRENT_ITINERARY", response.data);
-        this.isLoading = false;
-      });
+        .getItinerary(this.$route.params.itineraryId)
+        .then((response) => {
+          this.$store.commit("SET_CURRENT_ITINERARY", response.data);
+          this.isLoading = false;
+        });
     },
   },
   created() {
     this.getUserItinerary();
+    this.$store.commit("USER_IN_ITINERARY")
   },
+
+ 
+
   computed: {
     itinerary() {
-        return this.$store.state.itinerary;
-      },
-  },
-}
+      return this.$store.state.itinerary;
+    },
+   },
+   unmounted () {
+     this.$store.commit("USER_IN_ITINERARY")
+   }
+};
 </script>
 
 <style>
-
 </style>
