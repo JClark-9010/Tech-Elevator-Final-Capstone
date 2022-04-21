812f0a4eb13c2176645449c99435c1df80ddb890
Awel Alin <awel337@gmail.com>
2022-04-20 22:11:40 -0400

    personalize register and login page

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,44 +1,94 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
-      <h2 class="h3 mb-3 font-weight-normal">Please Sign In</h2>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <div id="buttons">
+  <section class="vh-100">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-lg-12 col-xl-11">
+          <div class="card text-black" style="border-radius: 25px">
+            <div class="card-body p-md-5">
+              <div class="row justify-content-center">
+                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                  <form class="form-signin" @submit.prevent="login">
+                    
+                    <h2 class="h3 mb-3 font-weight-normal">Please Sign In</h2>
+                    <div
+                      class="alert alert-danger"
+                      role="alert"
+                      v-if="invalidCredentials"
+                    >
+                      Invalid username and password!
+                    </div>
+                    <div
+                      class="alert alert-success"
+                      role="alert"
+                      v-if="this.$route.query.registration"
+                    >
+                      Thank you for registering, please sign in.
+                    </div>
 
-      <router-link id="signIn" :to="{ name: 'register' }">Need an account?</router-link>
-      <button  type="submit">Sign in</button>
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="username" class="sr-only">Username</label>
+                        <input
+                          type="text"
+                          id="username"
+                          class="form-control"
+                          placeholder="Username"
+                          v-model="user.username"
+                          required
+                          autofocus
+                        />
+                      </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="password" class="sr-only">Password</label>
+                        <input
+                          type="password"
+                          id="password"
+                          class="form-control"
+                          placeholder="Password"
+                          v-model="user.password"
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div
+                      class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
+                    >
+                      <router-link id="signIn" :to="{ name: 'register' }"
+                        >Need an account?</router-link
+                      >
+                      <button type="submit">Sign in</button>
+                    </div>
+                  </form>
+                </div>
+                <div
+                  class="
+                    col-md-10 col-lg-6 col-xl-7
+                    d-flex
+                    align-items-center
+                    order-1 order-lg-2
+                  "
+                >
+                  <img
+                    src="https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/columbus/scioto_mile_0052c2_51c16c33-b7d4-44c9-ac13-26f47e88f018.jpg"
+                    class="img-fluid"
+                    alt="Sample image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </form>
-  </div>
+    </div>
+  </section>
 </template>
+
 
 <script>
 import authService from "../services/AuthService";
@@ -50,50 +100,47 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style>
-#login{
+#login {
   margin-right: 35%;
   margin-left: 35%;
 }
-#signIn{
+#signIn {
   padding-right: 10px;
-  color: #1A5270;
+  color: #1a5270;
 }
-h2{
-  color:#1A5270;
+h2 {
+  color: #1a5270;
 }
-#buttons{
+#buttons {
   padding-top: 10px;
-  
-  
 }
-
 </style>
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,75 +1,133 @@
 <template>
-  <div id="register" class="text-center">
-    <form class="form-register" @submit.prevent="register">
-      <h2 class="h3 mb-3 font-weight-normal">Create Account</h2>
-      <div class="alert alert-danger" role="alert" v-if="registrationErrors">
-        {{ registrationErrorMsg }}
-      </div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-        placeholder="Confirm Password"
-        v-model="user.confirmPassword"
-        required
-      />
-      <router-link id="account" v-bind:to="{ name: 'Login' }">Have an account?</router-link>
-      <input id="submit" type="submit" value ="Create Account" />
-  
+  <section class="vh-100">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-lg-12 col-xl-11">
+          <div class="card text-black" style="border-radius: 25px">
+            <div class="card-body p-md-5">
+              <div class="row justify-content-center">
+                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                  <form class="form-register" @submit.prevent="register">
+                    
+                    <h2 class="h3 mb-3 font-weight-normal">Create Account</h2>
 
-    </form>
-  </div>
+                    <div
+                      class="alert alert-danger"
+                      role="alert"
+                      v-if="registrationErrors"
+                    >
+                      {{ registrationErrorMsg }}
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="username" class="sr-only">Username</label>
+                        <input
+                          type="text"
+                          id="username"
+                          class="form-control"
+                          placeholder="Username"
+                          v-model="user.username"
+                          required
+                          autofocus
+                        />
+                      </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="password" class="sr-only">Password</label>
+                        <input
+                          type="password"
+                          id="password"
+                          class="form-control"
+                          placeholder="Password"
+                          v-model="user.password"
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <label for="password" class="sr-only">Password</label>
+                        <input
+                          type="password"
+                          id="confirmPassword"
+                          class="form-control"
+                          placeholder="Confirm Password"
+                          v-model="user.confirmPassword"
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div
+                      class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
+                    >
+                      <router-link id="account" v-bind:to="{ name: 'login' }"
+                        >Have an account?</router-link
+                      >
+                      <input id="submit" type="submit" value="Create Account" />
+                    </div>
+                  </form>
+                </div>
+                <div
+                  class="
+                    col-md-10 col-lg-6 col-xl-7
+                    d-flex
+                    align-items-center
+                    order-1 order-lg-2
+                  "
+                >
+                  <img
+                    src="https://livability.com/wp-content/uploads/2018/11/ColumbusOH-ShortNorth1.jpg"
+                    class="img-fluid"
+                    alt="Sample image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -77,34 +135,35 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
 </script>
 
 <style scoped>
-input, #account{
-  margin: 15px
+input,
+#account {
+  margin: 15px;
 }
-#register{
+#register {
   margin-left: 35%;
   margin-right: 35%;
 }
-#submit{
-  background-color: #208B77;
-  border:black;
+#submit {
+  background-color: #208b77;
+  border: black;
   border-radius: 5px;
-  color:#EAD6C7;
+  color: #ead6c7;
 }
-#submit:hover{
- text-decoration: underline;
+#submit:hover {
+  text-decoration: underline;
 }
 </style>
